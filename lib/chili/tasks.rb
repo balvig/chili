eef756b17fb8661e72022f4ecfa1168e67e985d6
Jens Balvig <jens@balvig.com>
2012-05-09 12:44:04 +0900

    Took out rdoc stuff for now

--- lib/chili/tasks.rb
+++ lib/chili/tasks.rb
@@ -7,23 +7,6 @@ module Chili
       puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
     end
 
-    # Rdoc
-    begin
-      require 'rdoc/task'
-    rescue LoadError
-      require 'rdoc/rdoc'
-      require 'rake/rdoctask'
-      RDoc::Task = Rake::RDocTask
-    end
-
-    RDoc::Task.new(:rdoc) do |rdoc|
-      rdoc.rdoc_dir = 'rdoc'
-      rdoc.title    = 'CommentsExtension'
-      rdoc.options << '--line-numbers'
-      rdoc.rdoc_files.include('README.rdoc')
-      rdoc.rdoc_files.include('lib/**/*.rb')
-    end
-
     # App tasks
     require 'rails'
     load 'rails/tasks/engine.rake'
