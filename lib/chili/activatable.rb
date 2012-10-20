14ff0b91d7df629834a23b6376927d1d4fa4b042
Jens Balvig <jens@balvig.com>
2012-10-20 12:29:16 +0900

    Reload active_if on every request in dev mode.

--- lib/chili/activatable.rb
+++ lib/chili/activatable.rb
@@ -1,5 +1,9 @@
 module Chili
   module Activatable
+    def self.extended(base)
+      base.send(:unloadable) if base.respond_to?(:unloadable)
+    end
+
     def active_if(&block)
       @active_if = block
     end
